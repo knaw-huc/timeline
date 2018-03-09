17e674d033dec510a357eac163ac18626c44768b
Gijsjan <agijsbro@gmail.com>
2018-03-09 16:30:53 +0100

    Bump deps

--- build/src/models/events.domain.d.ts
+++
@@ -1,8 +0,0 @@
-import Domain from './domain';
-import Ev3nt from './event';
-import { RawEv3nt } from './config';
-import DomainConfig from './domain.config';
-export default class EventsDomain extends Domain {
-    events: Ev3nt[];
-    constructor(domain: DomainConfig, events: RawEv3nt[]);
-}
