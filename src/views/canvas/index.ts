a9aa8e51cf731b5807eef751adb5e239591f3c43
Gijsjan <agijsbro@gmail.com>
2018-07-31 16:42:23 +0200

    Use extra canvases for static minimaps and indicators

--- src/views/canvas/index.ts
+++ src/views/canvas/index.ts
@@ -1,12 +1,12 @@
 import createElement from '../../utils/create-element'
 import props from '../../models/props'
 import { DATE_BAR_HEIGHT, Pixels, EVENT_HEIGHT } from '../../constants'
-import { findClosestRulerDate, onVisible } from '../../utils'
+import { findClosestRulerDate } from '../../utils'
 import { labelBody } from '../../utils/dates'
 import MinimapBand from '../../models/band/minimap'
-import { DomainConfig, MinimapDomainConfig, EventsDomainConfig } from '../../models/config'
-import Band from '../../models/band'
-import animator from '../../animator';
+import { MinimapDomainConfig } from '../../models/config'
+import animator from '../../animator'
+import EventsBand from '../../models/band/events'
 
 /**
  * The MiniMap is an abstract representation of the events on a band.
@@ -17,14 +17,14 @@ export default class Canvas {
 	private canvas: HTMLCanvasElement
 	private ctx: CanvasRenderingContext2D
 
-	private offsiteCanvas: HTMLCanvasElement
+	private offsiteCanvas: HTMLCanvasElement = createElement('canvas')
 	private offsiteCtx: CanvasRenderingContext2D
 
+	private indicatorsCanvas: HTMLCanvasElement
+	private indicatorsCtx: CanvasRenderingContext2D
 
 	constructor() {
 		animator.registerViewUpdaters(this.update)
-
-		this.offsiteCanvas = createElement('canvas')
 		this.offsiteCtx = this.offsiteCanvas.getContext('2d')
 	}
 
@@ -38,77 +38,127 @@ export default class Canvas {
 		this.ctx = this.canvas.getContext('2d')
 		this.ctx.font = this.font
 
+		this.indicatorsCanvas = createElement('canvas', 'minimap', [
+			'position: absolute',
+		], [ 'z-index: 1' ])
+
+		this.indicatorsCanvas.width = props.viewportWidth
+		this.indicatorsCanvas.height = props.viewportHeight
+		this.indicatorsCtx = this.indicatorsCanvas.getContext('2d')
+
 		this.update()
 
-		return this.canvas
+		this.drawStaticMinimapBands()
+
+		return [this.canvas, this.indicatorsCanvas]
+	}
+
+	private drawStaticMinimapBands() {
+		props.minimapBands
+			.filter(band => band.zoomLevel === 0)
+			.forEach(band => {
+				band.domains.forEach(domain => {
+					this.drawMinimap(band, domain)
+				})
+
+				this.drawRulers(band)
+			})
+
+	}
+
+	// Clear the canvas active parts
+	private clear = () => {
+		// Clear the events band
+		this.ctx.clearRect(props.eventsBand.top, 0, this.canvas.width, props.eventsBand.height)
+
+		// Clear the minimap bands which are not totally zoomed out.
+		// If zoom level is 0, the band cannot move (it is completely visible),
+		// so it does not have to be recalculated and updated
+		props.minimapBands
+			.filter(band => band.zoomLevel !== 0)
+			.forEach(band => this.ctx.clearRect(0, band.top, this.canvas.width, band.height))
 	}
 
 	update = () => {
-		if (this.canvas.width !== props.viewportWidth) this.canvas.width = props.viewportWidth
-		if (this.canvas.height !== props.viewportHeight) this.canvas.height = props.viewportHeight
+		if (this.canvas.width !== props.viewportWidth || this.canvas.height !== props.viewportHeight) {
+			this.canvas.width = props.viewportWidth
+			this.canvas.height = props.viewportHeight
+			this.indicatorsCanvas.width = props.viewportWidth
+			this.indicatorsCanvas.height = props.viewportHeight
+			
+			this.drawStaticMinimapBands()
+		}
 
-		// TODO Clear rect only the bands where zoomLevel > 0
-		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+		this.clear()
 
+		this.drawRulers(props.eventsBand)
+
+		this.drawEvents()
+
+		props.minimapBands
+			.filter(band => band.zoomLevel !== 0)
+			.forEach(band => {
+				band.domains.forEach(domain => {
+					this.drawMinimap(band, domain)
+				})
+
+				this.drawRulers(band)
+			})
+
+		this.drawIndicators()
+	}
+
+	private drawEvents() {
 		this.ctx.beginPath()
 
 		props.eventsBand.domains.forEach(domain => {
+			const band = props.eventsBand
+			const left = band.positionAtTimestamp(band.from)
+			const offsetY = domain.topOffsetRatio * props.viewportHeight
+			const domainHeight = (domain.heightRatio * props.viewportHeight) - DATE_BAR_HEIGHT
 
-			if (domain.rulers) this.drawRulers(props.eventsBand, domain)
+			for (const event of domain.orderedEvents.events) {
+				event.left = band.positionAtTimestamp(event.from) - left
+				event.width = Math.round((event.time + event.space) * band.pixelsPerMillisecond)
+				if (event.width < 1) event.width = 1
+				event.top = offsetY + domainHeight - ((event.row + 1) * (EVENT_HEIGHT + 2))
+
+				if (event.from > band.to || event.to < band.from) continue
+
+				if (!event.time) {
+					this.ctx.moveTo(event.left, event.top + EVENT_HEIGHT/2)
+					this.ctx.arc(event.left, event.top + EVENT_HEIGHT/2, EVENT_HEIGHT/3, 0, 2 * Math.PI)
+				} else {
+					this.ctx.rect(event.left, event.top, event.width, EVENT_HEIGHT)
+				}
+			}
 		})
 
-		props.minimapBands.forEach(band => {
-			band.domains.forEach(domain => {
-				this.drawMinimap(band, domain)
-				if (domain.rulers) this.drawRulers(band, domain)
-				this.drawIndicators(band, domain)
-			})
-		})
+		this.ctx.fillStyle = `rgba(126, 0, 0, .3)`
+		this.ctx.fill()
 
-
-		this.ctx.closePath()
-
-		props.eventsBand.domains.forEach(domain => {
-			this.drawEvents(domain)
-		})
-
-		this.ctx.fillStyle = `rgba(255, 0, 0, .5)`
-		const x = props.eventsBand.positionAtTimestamp(props.eventsBand.timestampAtProportion(props.center)) + props.eventsBand.left
-		this.ctx.fillRect(x - 1, 0, 2, props.viewportHeight)
+		this.drawEventsText()
 	}
 
-	private drawEvents(domain: EventsDomainConfig) {
-		const band = props.eventsBand
-		const left = band.positionAtTimestamp(band.from)
-		const { events } = domain.orderedEvents
-		const visibleEvents = events.filter(onVisible(band.from, props.eventsBand.to))
-		const offsetY = domain.topOffsetRatio * props.viewportHeight
-		const domainHeight = (domain.heightRatio * props.viewportHeight) - DATE_BAR_HEIGHT
+	private drawEventsText() {
+		this.ctx.fillStyle = `rgb(126, 0, 0)`
+		for (const domain of props.eventsBand.domains) {
+			for (const event of domain.orderedEvents.events) {
+				if (event.from > props.eventsBand.to || event.to < props.eventsBand.from) continue
 
-		for (const event of visibleEvents) {
-			const eventWidth = Math.round((event.time + event.space) * band.pixelsPerMillisecond)
-			let eventLeft = band.positionAtTimestamp(event.from) - left
-			const y = offsetY + domainHeight - ((event.row + 1) * (EVENT_HEIGHT + 2))
-			let width = eventWidth < 1 ? 1 : eventWidth
+				let eventWidth = event.width
+				let eventLeft = event.left
 
-			this.ctx.fillStyle = `rgba(126, 0, 0, .3)`
-			if (!event.time) {
-				this.ctx.beginPath()
-				this.ctx.arc(eventLeft, y + EVENT_HEIGHT/2, EVENT_HEIGHT/3, 0, 2 * Math.PI)
-				this.ctx.fill()
-				this.ctx.closePath()
-			} else {
-				this.ctx.fillRect(eventLeft, y, width, EVENT_HEIGHT)
-			}
+				if (event.left < 0 && event.time !== 0) {
+					eventWidth = event.width + event.left
+					eventLeft = 0
+				}
 
-			if (eventLeft < 0) {
-				width = width + eventLeft
-				eventLeft = 0
-			}
-			if (this.ctx.measureText(event.label).width + 10 < width) {
-				this.ctx.fillStyle = `rgb(126, 0, 0)`
-				const paddingLeft = event.time ? 3 : 8
-				this.ctx.fillText(event.label, eventLeft + paddingLeft, y + EVENT_HEIGHT - 3)
+				if (event.label == null) event.label = 'NO LABEL'
+				if ((event.label.length * 8) + 10 < eventWidth) {
+					const paddingLeft = event.time ? 3 : 8
+					this.ctx.fillText(event.label, eventLeft + paddingLeft, event.top + EVENT_HEIGHT - 3)
+				}
 			}
 		}
 	}
@@ -136,57 +186,80 @@ export default class Canvas {
 		}
 
 		function drawEvents(ctx: CanvasRenderingContext2D, height, offsetY = band.top) {
+			ctx.beginPath()
 			const left = band.positionAtTimestamp(band.from)
-			ctx.fillStyle = `rgba(0, 0, 0, .2)`
 
 			domain.targets.forEach(targetIndex => {
 				const targetDomain = props.eventsBand.domains[targetIndex]
-				// this.ctx.fillStyle = targetDomain.color(.5)
 
-				const { events } = targetDomain.orderedEvents
-				const visibleEvents = events.filter(onVisible(band.from, band.to))
-
-				for (const event of visibleEvents) {
+				for (const event of targetDomain.orderedEvents.events) {
+					if (event.from > band.to || event.to < band.from) continue
 					const eventWidth = Math.round(event.time * band.pixelsPerMillisecond)
 					const eventLeft = band.positionAtTimestamp(event.date_min != null ? event.date_min : event.date)
 					const y = offsetY + height - ((event.row + 1) * eventHeight)
 					const width = eventWidth < 1 ? 1 : eventWidth
-					ctx.fillRect(eventLeft - left, y, width, eventHeight)
+					ctx.rect(eventLeft - left, y, width, eventHeight)
 				}
 			})
+
+			ctx.fillStyle = `rgba(0, 0, 0, .2)`
+			ctx.fill()
 		}
 	}
 
-	private drawIndicators(band: MinimapBand, domain: MinimapDomainConfig) {
-		this.ctx.fillStyle = `rgba(0, 0, 0, .1)`
+	private drawIndicators() {
+		this.indicatorsCtx.clearRect(0, 0, props.viewportWidth, props.viewportHeight)
 
-		// Left indicator
-		const indicatorTOP = Math.round(domain.topOffsetRatio * props.viewportHeight)
-		const leftIndicatorRightX = band.positionAtTimestamp(props.eventsBand.from) + band.left
-		this.ctx.fillRect(0, indicatorTOP, leftIndicatorRightX, band.height)
+		this.indicatorsCtx.beginPath()
 
-		// Right indicator
-		const rightIndicatorLeftX = band.positionAtTimestamp(props.eventsBand.to) + band.left
-		this.ctx.fillRect(rightIndicatorLeftX, indicatorTOP, props.viewportWidth, band.height)
+		props.minimapBands.forEach(band => {
+			band.domains.forEach(domain => {
+				// Left indicator
+				const indicatorTOP = Math.round(domain.topOffsetRatio * props.viewportHeight)
+				const leftIndicatorRightX = band.positionAtTimestamp(props.eventsBand.from) + band.left
+				this.indicatorsCtx.rect(0, indicatorTOP, leftIndicatorRightX, band.height)
 
-		// Cover the DATE_BAR
-		this.ctx.fillRect(leftIndicatorRightX, indicatorTOP + band.height - DATE_BAR_HEIGHT, rightIndicatorLeftX - leftIndicatorRightX, DATE_BAR_HEIGHT)
+				// Right indicator
+				const rightIndicatorLeftX = band.positionAtTimestamp(props.eventsBand.to) + band.left
+				this.indicatorsCtx.rect(rightIndicatorLeftX, indicatorTOP, props.viewportWidth, band.height)
+
+				// Cover the DATE_BAR
+				this.indicatorsCtx.rect(leftIndicatorRightX, indicatorTOP + band.height - DATE_BAR_HEIGHT, rightIndicatorLeftX - leftIndicatorRightX, DATE_BAR_HEIGHT)
+			})
+		})
+
+		this.indicatorsCtx.fillStyle = `rgba(0, 0, 0, .1)`
+		this.indicatorsCtx.fill()
+
+		// Draw red center line
+		this.indicatorsCtx.fillStyle = `rgba(255, 0, 0, .5)`
+		const x = props.eventsBand.positionAtTimestamp(props.eventsBand.timestampAtProportion(props.center)) + props.eventsBand.left
+		this.indicatorsCtx.fillRect(x - 1, 0, 2, props.viewportHeight)
+
+		this.indicatorsCtx.closePath()
 	}
 
-	private drawRulers(band: Band, domain: DomainConfig) {
-		this.ctx.strokeStyle = `rgb(200, 200, 200)`
+	private drawRulers(band: MinimapBand | EventsBand) {
+		this.ctx.beginPath()
 		this.ctx.fillStyle = `rgb(150, 150, 150)`
-		let date = findClosestRulerDate(band.from, band.granularity)
-		
-		const y = domain.topOffsetRatio * props.viewportHeight
-		const height = domain.heightRatio * props.viewportHeight
-		while(date < band.to) {
-			const left = band.positionAtTimestamp(date) + band.left
-			this.ctx.moveTo(left, y)
-			this.ctx.lineTo(left, y + height)
-			if (domain.rulerLabels) this.ctx.fillText(labelBody(date, band.granularity), left + 3, y + height - 3)
-			date = band.nextDate(date)
+
+		for (const domain of band.domains) {
+			if (!domain.rulers) continue
+
+			let date = findClosestRulerDate(band.from, band.granularity)
+			const y = domain.topOffsetRatio * props.viewportHeight
+			const height = domain.heightRatio * props.viewportHeight
+
+			while(date < band.to) {
+				const left = band.positionAtTimestamp(date) + band.left
+				this.ctx.moveTo(left, y)
+				this.ctx.lineTo(left, y + height)
+				if (domain.rulerLabels) this.ctx.fillText(labelBody(date, band.granularity), left + 3, y + height - 3)
+				date = band.nextDate(date)
+			}
 		}
+
+		this.ctx.strokeStyle = `rgb(200, 200, 200)`
 		this.ctx.stroke()
 	}
 }
